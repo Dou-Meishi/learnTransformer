de99ba8b929d67b5a185b53097b5851b6a86f1ae
Dou Meishi <doumeishid@gmail.com>
2024-11-09 11:58:46 +0800

    Test MyBatchNorm1d.

--- test.py
+++ test.py
@@ -201,3 +201,44 @@ print(f"Matched expected shape: {attn.shape == expected_attn.shape}")
 
 print("Error of output", (attn - expected_attn).abs().max().item())
 print(f"Matched expected output: {torch.allclose(attn, expected_attn, atol=1e-6)}")
+
+# %% [markdown]
+# ## Appendix: Benchamark Batchnorm
+
+# %%
+from normalization import MyBatchNorm1d
+print("="*60)
+print("Benchmark batch normalization") 
+
+# %%
+print("\nBatchNorm1d.")
+
+batch_norm = nn.BatchNorm1d(5)
+my_batch_norm = MyBatchNorm1d(5)
+my_batch_norm.load_from_pytorch_module(batch_norm)
+
+print("Training phase.")
+batch_norm.train(True)
+my_batch_norm.train(True)
+for _ in range(100):
+    # train a batch
+    x = torch.randn(16, 5)
+    y = my_batch_norm(x)
+    expected_y = batch_norm(x)
+
+    assert torch.allclose(y, expected_y, atol=1e-6), f"{(y - expected_y).abs().max()}"
+
+print("No error. Pass.")
+
+print("Evaluation phase.")
+batch_norm.train(False)
+my_batch_norm.train(False)
+for _ in range(100):
+    # predict a sample
+    x = torch.randn(1, 5)
+    y = my_batch_norm(x)
+    expected_y = batch_norm(x)
+
+    assert torch.allclose(y, expected_y, atol=1e-6), f"{(y - expected_y).abs().max()}"
+
+print("No error. Pass.")
